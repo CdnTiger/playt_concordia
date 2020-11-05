63d61553416326d25325b13d66eb75cc66268c84
CdnTiger <73088522+CdnTiger@users.noreply.github.com>
2020-11-04 22:49:57 -0600

    Update playtStatic.js

--- playtStatic.js
+++ playtStatic.js
@@ -5,18 +5,18 @@ var playt = {};
     playt.cards = [
         {   name: "playing_cards",
             include: 1,
-            cardBack: "cards/playing_cards.jpg",
+            cardBack: "cards/playing_cards.JPG",
             images: [
-                "cards/playing_cards/architect1.png",
-                "cards/playing_cards/architect2.png",
-                "cards/playing_cards/architect3.png",
-                "cards/playing_cards/architect4.png",
-                "cards/playing_cards/prefect1.png",
-                "cards/playing_cards/prefect2.png",
-                "cards/playing_cards/prefect3.png",
-                "cards/playing_cards/prefect4.png",
-                "cards/playing_cards/senator1.png",
-                "cards/playing_cards/senator2.png",
+                "cards/playing_cards/architect1.PNG",
+                "cards/playing_cards/architect2.PNG",
+                "cards/playing_cards/architect3.PNG",
+                "cards/playing_cards/architect4.PNG",
+                "cards/playing_cards/prefect1.PNG",
+                "cards/playing_cards/prefect2.PNG",
+                "cards/playing_cards/prefect3.PNG",
+                "cards/playing_cards/prefect4.PNG",
+                "cards/playing_cards/senator1.PNG",
+                "cards/playing_cards/senator2.PNG",
                 ]
         }
     ];
