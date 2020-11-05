7ff6bf331f3c87d08750a38ad2fe7060ee4316ca
CdnTiger <73088522+CdnTiger@users.noreply.github.com>
2020-11-04 21:36:37 -0600

    Add files via upload

---
+++ playtStatic.js
@@ -0,0 +1,48 @@
+var playt = {};
+
+(function() {
+
+    playt.cards = [
+        {   name: "playing_cards",
+            include: 1,
+            cardBack: "cards/playing_cards.jpg",
+            images: [
+                "cards/playing_cards/architect1.png",
+                "cards/playing_cards/architect2.png",
+                "cards/playing_cards/architect3.png",
+                "cards/playing_cards/architect4.png",
+                "cards/playing_cards/prefect1.png",
+                "cards/playing_cards/prefect2.png",
+                "cards/playing_cards/prefect3.png",
+                "cards/playing_cards/prefect4.png",
+                "cards/playing_cards/senator1.png",
+                "cards/playing_cards/senator2.png",
+                ]
+        }
+    ];
+    
+    playt.dice = [
+        {   name: "d6",
+            include: 5,
+            images: [
+                "dice/d6/dice-1-th.png",
+                "dice/d6/dice-2-th.png",
+                "dice/d6/dice-3-th.png",
+                "dice/d6/dice-4-th.png",
+                "dice/d6/dice-5-th.png",
+                "dice/d6/dice-6-th.png"
+            ]
+        },
+        {   name: "cats4",
+            include: 0,
+            images: [
+                "dice/cats4/cat1.png",
+                "dice/cats4/cat2.png",
+                "dice/cats4/cat3.png",
+                "dice/cats4/cat4.png",
+                "dice/cats4/cat5.png",
+                "dice/cats4/cat6.png"
+            ]
+        }
+    ];
+})();
