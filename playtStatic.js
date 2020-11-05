017dd09d15ca48c6009b6db87bb7f1ac3e2aaa75
CdnTiger <73088522+CdnTiger@users.noreply.github.com>
2020-11-04 21:58:41 -0600

    Update playtStatic.js

--- playtStatic.js
+++ playtStatic.js
@@ -17,7 +17,32 @@ var playt = {};
                 "cards/playing_cards/prefect4.png",
                 "cards/playing_cards/senator1.png",
                 "cards/playing_cards/senator2.png",
-                ]
+            ]
+        }
+    ];
+    
+    playt.dice = [
+        {   name: "d6",
+            include: 5,
+            images: [
+                "dice/d6/dice-1-th.png",
+                "dice/d6/dice-2-th.png",
+                "dice/d6/dice-3-th.png",
+                "dice/d6/dice-4-th.png",
+                "dice/d6/dice-5-th.png",
+                "dice/d6/dice-6-th.png"
+            ]
+        },
+        {   name: "cats4",
+            include: 0,
+            images: [
+                "dice/cats4/cat1.png",
+                "dice/cats4/cat2.png",
+                "dice/cats4/cat3.png",
+                "dice/cats4/cat4.png",
+                "dice/cats4/cat5.png",
+                "dice/cats4/cat6.png"
+            ]
         }
     ];
 })();
